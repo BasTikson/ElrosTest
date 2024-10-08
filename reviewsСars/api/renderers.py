bbf5212c5051c006211fe14609205b6e10f7cc89
tikson <workmessegetb@gmail.com>
2024-10-08 16:00:52 +0300

    Export file

---
+++ reviewsСars/api/renderers.py
@@ -0,0 +1,39 @@
+import io
+import csv
+from rest_framework import renderers
+import openpyxl
+
+
+class CSVStudentDataRenderer(renderers.BaseRenderer):
+    media_type = "text/csv"
+    format = "csv"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        csv_buffer = io.StringIO()
+        headers = list(data[0].keys())
+        csv_writer = csv.DictWriter(csv_buffer, fieldnames=headers, extrasaction="ignore")
+        csv_writer.writeheader()
+
+        for item in data:
+            csv_writer.writerow(item)
+
+        return csv_buffer.getvalue()
+
+
+class ExcelStudentDataRenderer(renderers.BaseRenderer):
+
+    media_type = "application/vnd.ms-excel"
+    format = "xlsx"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+
+        workbook = openpyxl.Workbook()
+        buffer = io.BytesIO()
+        worksheet = workbook.active
+        headers = list(data[0].keys())
+        worksheet.append(headers)
+        for item in data:
+            worksheet.append([str(value) for value in item.values()])
+        workbook.save(buffer)
+
+        return buffer.getvalue()
--- reviewsСars/api/views.py
+++ reviewsСars/api/views.py
@@ -4,16 +4,13 @@ from .serializers.serializers_countrys import  CountrySerializer
 from .serializers.serializers_manufacturers import ManufacturersSerializer
 from .serializers.serializers_cars import CarsSerializer
 from .serializers.serializers_comments import CommentsSerializer
-import csv
-from io import StringIO
-from openpyxl import Workbook
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import Country
-
+from django.utils import timezone
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from .renderers import CSVStudentDataRenderer, ExcelStudentDataRenderer
 
 class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -33,66 +30,22 @@ class IsAuthenticatedComments(permissions.BasePermission):
         return request.user and request.user.is_authenticated
 
 
+
+
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    @action(detail=False, methods=['GET'])
+    @action(detail=False, methods=["get"], renderer_classes=[CSVStudentDataRenderer, ExcelStudentDataRenderer])
     def download_file(self, request):
-        # Получаем параметр format из запроса
-        file_format = request.query_params.get('format', 'csv').lower()
-
-        # Получаем все объекты Country
-        countries = self.get_queryset()
-        # Сериализуем данные
-        serializer = self.get_serializer(countries, many=True)
-        data = serializer.data
-
-
-        if file_format == 'csv':
-            # Создаем CSV файл в памяти
-            csv_buffer = StringIO()
-            writer = csv.writer(csv_buffer)
-
-            # Заголовки CSV файла
-            header = data[0].keys() if data else []
-            writer.writerow(header)
-
-            # Записываем данные в CSV файл
-            for item in data:
-                writer.writerow(item.values())
-
-            # Возвращаем CSV файл в ответе
-            response = Response(csv_buffer.getvalue(), content_type='text/csv')
-            response['Content-Disposition'] = 'attachment; filename="countries.csv"'
-            return response
-
-        elif file_format == 'xlsx':
-            # Создаем XLSX файл в памяти
-            workbook = Workbook()
-            sheet = workbook.active
-
-            # Заголовки XLSX файла
-            header = data[0].keys() if data else []
-            sheet.append(header)
-
-            # Записываем данные в XLSX файл
-            for item in data:
-                sheet.append(item.values())
-
-            # Возвращаем XLSX файл в ответе
-            xlsx_buffer = StringIO()
-            workbook.save(xlsx_buffer)
-            xlsx_buffer.seek(0)
-
-            response = Response(xlsx_buffer.getvalue(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            response['Content-Disposition'] = 'attachment; filename="countries.xlsx"'
-            return response
-
-        else:
-            return Response({'error': 'Unsupported file format'}, status=400)
+        queryset = self.get_queryset()
+        now = timezone.now()
+        file_name = f"Country_{now:%Y-%m-%d_%H-%M-%S}.{request.accepted_renderer.format}"
+        serializer = CountrySerializer(queryset, many=True)
+        response = Response(serializer.data, headers={"Content-Disposition": f'attachment; filename="{file_name}"'})
+        return response
 
 
 class ManufacturersViewSet(viewsets.ModelViewSet):
@@ -101,11 +54,29 @@ class ManufacturersViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    @action(detail=False, methods=["get"], renderer_classes=[CSVStudentDataRenderer, ExcelStudentDataRenderer])
+    def download_file(self, request):
+        queryset = self.get_queryset()
+        now = timezone.now()
+        file_name = f"Country_{now:%Y-%m-%d_%H-%M-%S}.{request.accepted_renderer.format}"
+        serializer = ManufacturersSerializer(queryset, many=True)
+        response = Response(serializer.data, headers={"Content-Disposition": f'attachment; filename="{file_name}"'})
+        return response
+
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["get"], renderer_classes=[CSVStudentDataRenderer, ExcelStudentDataRenderer])
+    def download_file(self, request):
+        queryset = self.get_queryset()
+        now = timezone.now()
+        file_name = f"Country_{now:%Y-%m-%d_%H-%M-%S}.{request.accepted_renderer.format}"
+        serializer = CarsSerializer(queryset, many=True)
+        response = Response(serializer.data, headers={"Content-Disposition": f'attachment; filename="{file_name}"'})
+        return response
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
@@ -113,5 +84,13 @@ class CommentsViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedComments]
 
+    @action(detail=False, methods=["get"], renderer_classes=[CSVStudentDataRenderer, ExcelStudentDataRenderer])
+    def download_file(self, request):
+        queryset = self.get_queryset()
+        now = timezone.now()
+        file_name = f"Country_{now:%Y-%m-%d_%H-%M-%S}.{request.accepted_renderer.format}"
+        serializer = CommentsSerializer(queryset, many=True)
+        response = Response(serializer.data, headers={"Content-Disposition": f'attachment; filename="{file_name}"'})
+        return response
 
-# ToDo: Функционал скачивания при GET запросе в указанном формате.
+
